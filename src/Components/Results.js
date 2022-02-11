b3a5067058081144da173db2ab5af41510cc9cd3
RajPatel <raj191@live.com>
2022-02-10 22:29:03 -0500

    updating api to get generate battle scores

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,6 +1,13 @@
-import react from "react";
+import { useEffect } from "react";
+import { battle } from "../utils/api";
 
 const Results = (props) => {
+  useEffect(() => {
+    battle([props.playerOne, props.playerTwo]).then((players) => {
+      console.log("data: ", players);
+    });
+  });
+
   return (
     <div>
       Results
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,69 @@
+const id = "";
+const secretKey = "";
+const params = `client_id=${id}&client_secret=${secretKey}`;
+
+const getErrorMsg = (message, username) => {
+  if (message === "Not Found") {
+    return `${username} doesn't exist`;
+  }
+
+  return message;
+};
+
+const getProfile = (username) => {
+  return fetch(`https://api.github.com/users/${username}`)
+    .then((res) => res.json())
+    .then((profile) => {
+      if (profile.message) {
+        throw new Error(getErrorMsg(profile.message, username));
+      }
+
+      return profile;
+    });
+};
+
+const getRepos = (username) => {
+  return fetch(`https://api.github.com/users/${username}/repos`)
+    .then((res) => res.json())
+    .then((repos) => {
+      if (repos.message) {
+        throw new Error(getErrorMsg(repos.message, username));
+      }
+
+      return repos;
+    });
+};
+
+const getStarCount = (repos) => {
+  return repos.reduce(
+    (count, { stargazers_count }) => count + stargazers_count,
+    0
+  );
+};
+
+const calculateScore = (followers, repos) => {
+  return followers * 3 + getStarCount(repos);
+};
+
+const getUserData = (player) => {
+  return Promise.all([getProfile(player), getRepos(player)]).then(
+    ([profile, repos]) => ({
+      profile,
+      score: calculateScore(profile.followers, repos),
+    })
+  );
+};
+
+const sortPlayers = (players) => {
+  return players.sort((a, b) => b.score - a.score);
+};
+
+const battle = (players) => {
+  return Promise.all([getUserData(players[0]), getUserData(players[1])]).then(
+    (results) => sortPlayers(results)
+  );
+};
+
 const fetchPopularRepos = (language) => {
   const endpoint = window.encodeURI(
     `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
@@ -16,4 +82,4 @@ const fetchPopularRepos = (language) => {
   //handle this in the ui layer
 };
 
-export { fetchPopularRepos };
+export { fetchPopularRepos, battle };
