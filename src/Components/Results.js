203969b0bbcef8eff3e0d8775f10f455fb99cfc6
RajPatel <raj191@live.com>
2022-02-11 19:08:41 -0500

    adjusting results display

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,17 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { battle } from "../utils/api";
 
 const Results = (props) => {
+  const [state, setState] = useState({});
+  const [winner, setWinner] = useState(null);
+  const [loser, setLoser] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    battle([props.playerOne, props.playerTwo]).then((players) => {
-      console.log("data: ", players);
-    });
+    battle([props.playerOne, props.playerTwo])
+      .then((players) => {
+        // console.log("data: ", players);
+        setWinner(players[0]);
+        setLoser(players[1]);
+        setError(null);
+        setLoading(false);
+        setState({ winner, loser, error, loading });
+      })
+      .catch(({ message }) => {
+        setError(message);
+        setLoading(false);
+        setState({ error, loading });
+      });
   });
 
   return (
     <div>
       Results
-      <pre>{JSON.stringify(props, null, 2)}</pre>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
   );
 };
