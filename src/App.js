f271defa0af9ef6ed3611a8021869d7c48addb4b
RajPatel <raj191@live.com>
2022-02-15 12:49:04 -0500

    adding routes to app

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import Battle from "./Components/Battle";
 import Popular from "./Components/Popular";
 import ThemeContext from "./Contexts/theme";
 import Nav from "./Components/Nav";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [theme, setTheme] = useState("light");
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      <div className={theme}>
-        <div className="container">
-          <Nav />
-          <Battle />
-          {/* <Popular /> */}
+    <Router>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <div className={theme}>
+          <div className="container">
+            <Nav />
+            <Routes>
+              <Route exact path="/" element={<Popular />}>
+                {/* <Popular /> */}
+              </Route>
+              <Route path="/battle" element={<Battle />}>
+                {/* <Battle /> */}
+              </Route>
+            </Routes>
+          </div>
         </div>
-      </div>
-    </ThemeContext.Provider>
+      </ThemeContext.Provider>
+    </Router>
   );
 }
 
