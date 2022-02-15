afbee331462527d16254c46e11c3ef06578ff359
RajPatel <raj191@live.com>
2022-02-15 13:03:45 -0500

    adding links with styles

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,10 +1,35 @@
 import ThemeContext from "../Contexts/theme";
+import { NavLink } from "react-router-dom";
+
+const activeStyle = {
+  color: "rgb(187, 46, 31)",
+};
 
 const Nav = () => {
   return (
     <ThemeContext.Consumer>
       {({ theme, setTheme }) => (
         <nav className="row space-between">
+          <ul className="row nav">
+            <li>
+              <NavLink
+                to={"/"}
+                className="nav-link"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              >
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={"/battle"}
+                className="nav-link"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              >
+                Battle
+              </NavLink>
+            </li>
+          </ul>
           <button
             className="btn-clear"
             style={{ fontSize: 30 }}
